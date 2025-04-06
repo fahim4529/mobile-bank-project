935bc1f23dcb551986b60c2cec34190fcbc6c59e
fahim4529 <fahimfarhan685@gmail.com>
2025-04-06 15:20:29 +0600

    I complete my work

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
-document.getElementById("login-btn").addEventListener("click", function(){
-console.log('hello Fahim');
+document.getElementById("login-btn").addEventListener("click", function(event){
+event.preventDefault();
+// console.log('hello bangladesh')
+const accountNumber=document.getElementById('Account-number').value;
+// console.log("accountNumber");
+
+const pin = document.getElementById('pin').value;
+// console.log(accountNumber,pin);
+if(accountNumber.length===11){
+    // console.log("yess")
+    // console.log(typeof pin)
+    if (pin==='1234'){
+        // console.log('okey thik ace');
+        window.location.href="./main.html"
+    }
+    else{
+        alert('pin thik nai')
+    }
+}
+else{
+    alert("need valid number")
+}
+
 });
